dc8712d3497073777257870ae94009f0033a793a
Daniel Chafamo <chafamodaniel@gmail.com>
2021-03-31 11:33:44 -0400

    BWH-002 | static root updated

--- Graphs/views.py
+++ Graphs/views.py
@@ -1,30 +1,31 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.conf import settings
 import json
 import os
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-REPOSITORY_ROOT = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'NetVis/Graphs/static/')
+# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# REPOSITORY_ROOT = os.path.dirname(BASE_DIR)
+# STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'NetVis/Graphs/static/')
 
 
 # Create your views here.
 def Graph(request): 
-  fsource = os.path.join(STATIC_ROOT,'graphs/js/filter_feats.json')
+  fsource = os.path.join(settings.STATIC_ROOT,'graphs/js/filter_feats.json')
   context = {'feats': json.dumps(json.load(open(fsource)) ),
              'initial': request.GET.get('initial', -1)} 
   return render(request, 'graphs/graph.html', context=context)
 
 
 def Index(request): 
-  fsource = os.path.join(STATIC_ROOT,'graphs/js/filter_feats.json') 
+  fsource = os.path.join(settings.STATIC_ROOT,'graphs/js/filter_feats.json')
   context={'feats':json.dumps(json.load(open(fsource)) )}
   return render(request, 'graphs/index.html', context=context)
 
 
 def LoadJsoNet(request):
   pid = request.GET.get('pid', None)
-  source = os.path.join(STATIC_ROOT,'graphs/js/jsoNets/{}_'.format(pid))
+  source = os.path.join(settings.STATIC_ROOT,'graphs/js/jsoNets/{}_'.format(pid))
   context = {'g{}'.format(i): json.dumps(json.load(open(source+'{}m_graph.json'.format(i)))) for i in [0,3,6]}
   return JsonResponse(context)
