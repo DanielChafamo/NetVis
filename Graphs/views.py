569042cff112e18143619c17bc1e18ebe52d8214
Daniel Chafamo <chafamodaniel@gmail.com>
2021-05-02 09:36:14 -0400

    BWH-004 | allow xframe

--- Graphs/views.py
+++ Graphs/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.conf import settings
+from django.views.decorators.clickjacking import xframe_options_exempt
 import json
 import os
 
 
-# Create your views here.
+@xframe_options_exempt
 def Graph(request):
     fsource = os.path.join(settings.STATIC_ROOT, 'graphs/js/filter_feats.json')
     context = {'feats': json.dumps(json.load(open(fsource))),
@@ -13,12 +14,14 @@ def Graph(request):
     return render(request, 'graphs/graph.html', context=context)
 
 
+@xframe_options_exempt
 def Explore(request):
     fsource = os.path.join(settings.STATIC_ROOT, 'graphs/js/filter_feats.json')
     context = {'feats': json.dumps(json.load(open(fsource)))}
     return render(request, 'graphs/explore.html', context=context)
 
 
+@xframe_options_exempt
 def Grid(request):
     fsource = os.path.join(settings.STATIC_ROOT, 'graphs/js/filter_feats.json')
     feats = json.load(open(fsource))
@@ -30,10 +33,12 @@ def Grid(request):
     return render(request, 'graphs/grid.html', context=context)
 
 
+@xframe_options_exempt
 def About(request):
     return render(request, 'graphs/about.html')
 
 
+@xframe_options_exempt
 def LoadJsoNet(request):
     pid = request.GET.get('pid', None)
     source = os.path.join(settings.STATIC_ROOT, 'graphs/js/jsoNets/{}_'.format(pid))
